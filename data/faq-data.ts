8c776afedecb572e989d47f18488190362967e99
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-11 19:03:18 +0100

    updated the FAQ data file

--- data/faq-data.ts
+++ data/faq-data.ts
@@ -3,16 +3,16 @@ export const faqs = [
     {
       question: "What is Devjourney?",
       answer:
-        "DevJourney is a platform dedicated to Engineers and developers, where you can document and share your coding experiences, projects, and insights. It aims to foster a community of learning and collaboration among engineers and developers at all stages of their journey.",
+        "DevJourney is a platform dedicated to Engineers, developers and tech enthusiasts, where you can document and share your coding experiences, projects, and insights. It aims to foster a community of learning and collaboration among engineers and developers at all stages of their journey.",
     },
     {
       question: "How do I create an account?",
       answer:
-        "You can create an account by clicking the 'Signup' button on the homepage and following the registration process.",
+        "You can create an account by clicking the 'Signup' button on the homepage and following the registration process. This process uses GitHub OAuth for authentication. Therefore you must have a GitHub account to sign up.",
     },
     {
       question: "Can I edit my posts after publishing?",
-      answer: "Yes, you can edit your posts anytime after they are published.",
+      answer: "Yes, you can edit your posts anytime after they are published, however, this feature is currently undergoing some slit updates on it so not available to users at the moment.",
     },
     {
       question: "What are the community guidelines?",
@@ -22,12 +22,12 @@ export const faqs = [
     {
       question: "How do I report a problem?",
       answer:
-        "You can report any issues through the Contact page or by reaching out to me directly either via email or social media.",
+        "You can report any issues through the 'Report a Bug' button avialable at the button-right corner of the website as well as using the Contact page or by reaching out to me directly either via email or social media.",
     },
     {
       question: "How do I create a post?",
       answer:
-        "You can create a post by clicking the 'Share your experience' button on the homepage and following the post creation process.",
+        "You can create a post by clicking the 'Share your experience' button on the homepage and following the post creation process or by simply just clicking on the create button from the Home page of the website.",
     },
   ];
   
