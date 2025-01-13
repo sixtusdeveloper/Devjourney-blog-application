455346b48067bfd38836fd31443a9536623e1ab3
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-13 20:16:18 +0100

    updated the post detailed page

--- app/(root)/post/[id]/page.tsx
+++ app/(root)/post/[id]/page.tsx
@@ -129,7 +129,7 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
         <hr className="divider my-4" />
 
         {editorPosts?.length > 0 && (
-          <div className="space-y-5 mt-8 max-w-7xl sm:px-4 mx-auto">
+          <div className="space-y-5 my-10 max-w-7xl sm:px-4 mx-auto">
             <p className="text-30-semibold">Related Posts</p>
 
             <ul className="mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
